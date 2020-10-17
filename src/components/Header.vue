1dee3830810465d430bf1534497fc9412b8b7823
Martin Rivière <martin@360medics.com>
2020-10-17 19:08:28 +0200

    changes background image

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <g-image src="../assets/img/logo.png" id="logo" class="mb-16" />
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import CountDown from "~/components/CountDown.vue"
+
+export default Vue.extend({
+  components: { CountDown },
+})
+</script>
+
+<style>
+#logo {
+  width: 500px;
+}
+@media screen and (max-width: 767px) {
+  #logo {
+    width: 200px;
+  }
+}
+</style>
