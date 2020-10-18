47d39ac74af2c3de91a0ee7a6d960e1dc4171413
Martin Rivière <martin@360medics.com>
2020-10-18 04:50:27 +0200

    puts header in App.vue so it is not instancied each time route changes

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Header v-if="$route.path !== '/'" />
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import Header from "./components/Header.vue"
+
+export default Vue.extend({
+  components: { Header },
+})
+</script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,30 @@
 <template>
-    <ul
-      :class="[
-        mobileVisible
-          ? 'transform translate-x-0'
-          : 'transform translate-x-full sm:translate-x-0',
-        'flex flex-col sm:flex-row sm:items-center sm:w-full px-4 py-2 sm:px-10 sm:py-5 bg-dark fixed sm:static top-0 bottom-0 right-0 transition-transform duration-500 ease-out',
-      ]"
+  <ul
+    :class="[
+      mobileVisible
+        ? 'transform translate-x-0'
+        : 'transform translate-x-full sm:translate-x-0',
+      'flex flex-col sm:flex-row sm:items-center sm:w-full px-4 py-2 sm:px-10 sm:py-5 bg-dark fixed sm:static top-0 bottom-0 right-0 transition-transform duration-500 ease-out',
+    ]"
+  >
+    <div
+      class="text-white text-3xl ml-auto mb-5 sm:hidden"
+      @click="$emit('close')"
     >
-      <div
-        class="text-white text-3xl ml-auto mb-5 sm:hidden"
-        @click="$emit('close')"
-      >
-        <FontAwesome :icon="['fas', 'times']" />
-      </div>
-      <li
-        v-for="item in pages"
-        :key="item.label"
-        :class="[
-          selected === item.link ? 'text-grey' : 'text-white',
-          'my-1 sm:my-0 sm:mr-5 text-xl',
-        ]"
-      >
-        <g-link :to="item.link">{{ item.label }}</g-link>
-      </li>
-    </ul>
+      <FontAwesome :icon="['fas', 'times']" />
+    </div>
+    <g-link
+      v-for="item in pages"
+      :to="item.link"
+      tag="li"
+      @click="$emit('close')"
+      :key="item.label"
+      class="my-1 sm:my-0 sm:mr-5 text-xl text-white"
+      active-class="text-grey"
+    >
+      <span @click="$emit('close')">{{ item.label }}</span>
+    </g-link>
+  </ul>
 </template>
 
 <script lang="ts">
@@ -48,12 +49,6 @@ export default Vue.extend({
     return {
       pages,
     }
-  },
-  computed: {
-    selected() {
-      // @ts-ignore
-      return this.$route.path
-    },
   },
 })
 </script>
