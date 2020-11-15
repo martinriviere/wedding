f3ce5098c9f7830f2e0d333a13de062f1a4af82d
Martin Rivière <martin@360medics.com>
2020-11-15 20:04:23 +0100

    possibility to make a call by clicking on number

--- src/components/Phone.vue
+++ src/components/Phone.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex flex-col items-center m-2">
     <p class="font-bold">{{ name }}</p>
-    <div class="flex items-center">
+    <a class="flex items-center" :href="`tel:${phone}`">
       <FontAwesome :icon="['fas', 'phone']" class="mr-2" />
-      <span>{{ phone }}</span>
-    </div>
+      <span>{{ phone }}</span></a
+    >
   </div>
 </template>
 
