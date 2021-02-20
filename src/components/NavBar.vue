6e842a4e171b8b39bb11ade6103bd9efba8c929a
Martin Rivière <martin@360medics.com>
2021-02-20 11:32:55 +0100

    💄 Fix UI issues

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,11 +2,11 @@
   <div
     :class="[
       mobileVisible ? 'right-0' : '-right-full',
-      'bg-dark fixed md:static top-0 bottom-0 transition-all duration-500 ease-out z-10',
+      'bg-dark fixed lg:static top-0 bottom-0 transition-all duration-500 ease-out z-10',
     ]"
   >
-    <div class="flex flex-col md:flex-row md:items-center px-4 py-2 md:py-5">
-      <div class="text-white text-3xl ml-auto mb-5 md:hidden" @click="$emit('close')">
+    <div class="flex flex-col lg:flex-row lg:items-center px-4 py-2 lg:py-5">
+      <div class="text-white text-3xl ml-auto mb-5 lg:hidden" @click="$emit('close')">
         <FontAwesome :icon="['fas', 'times']" />
       </div>
       <g-link
@@ -14,12 +14,12 @@
         :to="item.link"
         @click="$emit('close')"
         :key="item.label"
-        class="my-1 md:my-0 md:mr-5 text-white text-xl md:text-base"
+        class="my-1 lg:my-0 lg:mr-5 text-white text-xl lg:text-base"
         active-class="text-grey-light"
       >
         <span @click="$emit('close')">{{ item.label }}</span>
       </g-link>
-      <span class="my-1 md:my-0 md:mr-5 text-white text-xl md:text-base cursor-pointer" @click="logOut"
+      <span class="my-1 lg:my-0 lg:mr-5 text-white text-xl lg:text-base cursor-pointer" @click="logOut"
         >Déconnexion</span
       >
     </div>
