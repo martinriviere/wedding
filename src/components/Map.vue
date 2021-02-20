a4224ffe58355f965e827610616654e560f3e9ab
Martin Rivière <martin@360medics.com>
2021-02-20 19:10:34 +0100

    ✨ Mairie page

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,16 +2,26 @@
   <div class="map-container">
     <LMap :zoom="zoom" :center="center" :options="mapOptions">
       <LTileLayer :url="url" :options="layerOptions" />
-      <LMarker :lat-lng="center" />
+      <LMarker v-for="marker in importantPlaces" :icon="importantIcon" :lat-lng="marker.latLng" :key="marker.popup">
+        <LPopup>
+          <a :href="marker.link" :style="!marker.link && 'color: black;'" target="_blank">{{ marker.text }}</a>
+        </LPopup>
+      </LMarker>
+      <LMarker v-for="marker in markers" :lat-lng="marker.latLng" :key="marker.popup">
+        <LPopup>
+          <a :href="marker.link" :style="!marker.link && 'color: black;'" target="_blank">{{ marker.text }}</a>
+        </LPopup>
+      </LMarker>
     </LMap>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue"
-import { LMap, LTileLayer, LMarker } from "vue2-leaflet"
+import Vue, { PropType } from 'vue'
+import { LMap, LTileLayer, LMarker, LPopup } from 'vue2-leaflet'
 // @ts-ignore
-import { Icon } from "leaflet"
+import { Icon, LatLngLiteral } from 'leaflet'
+import { Marker } from '../utils/latLng'
 
 type D = Icon.Default & {
   _getIconUrl?: string
@@ -19,9 +29,10 @@ type D = Icon.Default & {
 
 delete (Icon.Default.prototype as D)._getIconUrl
 Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+  iconRetinaUrl: require('../assets/img/map/blue-icon-2x.png'),
+  iconUrl: require('../assets/img/map/blue-icon.png'),
+  iconSize: [35, 35],
+  shadowUrl: null,
 })
 
 export default Vue.extend({
@@ -29,27 +40,36 @@ export default Vue.extend({
     LMap,
     LTileLayer,
     LMarker,
+    LPopup,
+  },
+  props: {
+    center: Object as PropType<LatLngLiteral>,
+    importantPlaces: Array as PropType<Marker[]>,
+    markers: Array as PropType<Marker[]>,
+    zoom: { type: Number, default: 10 },
   },
   data() {
     return {
-      url:
-        "https://api.mapbox.com/styles/v1/martinriviere/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-      zoom: 10,
-      center: [44.729471, -0.421311],
+      url: 'https://api.mapbox.com/styles/v1/martinriviere/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
       bounds: null,
       mapOptions: {
         dragging: true,
-        scrollWheelZoom: false
+        scrollWheelZoom: false,
       },
       layerOptions: {
         attribution:
           "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
         accessToken:
-          "pk.eyJ1IjoibWFydGlucml2aWVyZSIsImEiOiJjazBubXBvczQwMWRpM2hwcGV4c3pubHJ1In0.qtYexkvHKigcd_hgl4Q9jA",
-        id: "ckhjdog8f4a2719kzj5z4kib3",
+          'pk.eyJ1IjoibWFydGlucml2aWVyZSIsImEiOiJjazBubXBvczQwMWRpM2hwcGV4c3pubHJ1In0.qtYexkvHKigcd_hgl4Q9jA',
+        id: 'ckhjdog8f4a2719kzj5z4kib3',
         tileSize: 512,
         zoomOffset: -1,
       },
+      importantIcon: new Icon({
+        iconRetinaUrl: require('../assets/img/map/red-icon-2x.png'),
+        iconUrl: require('../assets/img/map/red-icon.png'),
+        iconSize: [35, 35],
+      }),
     }
   },
 })
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,10 +11,10 @@
           </SectionTitle>
           <p class="my-4 text-2xl sm:text-4xl font-haviland">10 Juillet 2021</p>
           <p>
-            Nous avons créé ce site dédié à notre mariage pour que vous ayez
-            accès à toutes les informations pratiques<br />
-            Laissez-vous porter par la visite pour trouver ce qu'il vous faut
-            pour profiter pleinement de ce fabuleux évènement avec nous !
+            Nous avons créé ce site dédié à notre mariage pour que vous ayez accès à toutes les informations
+            pratiques<br />
+            Laissez-vous porter par la visite pour trouver ce qu'il vous faut pour profiter pleinement de ce fabuleux
+            évènement avec nous !
           </p>
         </div>
       </div>
@@ -27,29 +27,21 @@
           </SectionTitle>
           <div class="text-white">
             <p>
-              La cloche a sonné la fin des cours de la journée. Nous avions
-              quatorze ans, et nous nous dépêchions d’aller en salle d’études…
-              mais nous n’y sommes en fait jamais allés. Nous sommes restés tous
-              les deux et nous avons eu notre premier "rencard" devant le
-              collège de notre enfance, et la suite vous la connaissez. Tout le
-              monde dit que l’on sait quand on rencontre la bonne personne, et
-              c’est exactement ce qui nous est arrivé.
+              La cloche a sonné la fin des cours de la journée. Nous avions quatorze ans, et nous nous dépêchions
+              d’aller en salle d’études… mais nous n’y sommes en fait jamais allés. Nous sommes restés tous les deux et
+              nous avons eu notre premier "rencard" devant le collège de notre enfance, et la suite vous la connaissez.
+              Tout le monde dit que l’on sait quand on rencontre la bonne personne, et c’est exactement ce qui nous est
+              arrivé.
             </p>
             <p>
-              Et maintenant : 4 animaux de compagnies, 1 PACS, 1 appartement,
-              d’innombrables voyages et surtout un enfant. Nous sommes ravis de
-              passer le cap et de nous unir par les liens du mariage. Pour
-              passer cette grande étape, nous avons hâte de vous avoir à nos
-              côtés.
+              Et maintenant : 4 animaux de compagnies, 1 PACS, 1 appartement, d’innombrables voyages et surtout un
+              enfant. Nous sommes ravis de passer le cap et de nous unir par les liens du mariage. Pour passer cette
+              grande étape, nous avons hâte de vous avoir à nos côtés.
             </p>
           </div>
         </div>
         <div class="sm:w-1/2">
-          <g-image
-            src="../assets/img/main/us.jpg"
-            alt="Us"
-            class="sm:w-4/5 sm:m-10"
-          />
+          <g-image src="../assets/img/main/us.jpg" alt="Us" class="sm:w-4/5 sm:m-10" />
         </div>
       </div>
     </div>
@@ -64,23 +56,32 @@
         </div>
       </div>
     </div>
-    <Map />
+    <Map :center="center" :importantPlaces="markers" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue"
-import Map from "../components/Map.vue"
+import Vue from 'vue'
+import Map from '../components/Map.vue'
+import { generateMarker } from '../utils/latLng'
+
 export default Vue.extend({
   components: {
     Map,
+  },
+  data() {
+    return {
+      center: { lat: 44.729471, lng: -0.421311 },
+      // markers: [{ popup: 'Château Sentout, Tabanac', latLng: { lat: 44.729471, lng: -0.421311 } }],
+      markers: [generateMarker('Château Sentout, Tabanac', 44.72947056817649, -0.42128920438841)],
+    }
   },
 })
 </script>
 
 <style lang="scss" scoped>
 .flower {
-  background-image: url("../assets/img/flower.png");
+  background-image: url('../assets/img/flower.png');
   background-position: left -20px top -20px;
 }
 </style>
